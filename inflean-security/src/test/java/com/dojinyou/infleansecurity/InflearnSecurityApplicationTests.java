69310e0e77cae7ff3525da78a93730d13691b7fe
dojinyou <djyou128@gmail.com>
2022-10-21 20:29:39 +0900

    :tada: init inflearn security project

---
+++ inflean-security/src/test/java/com/dojinyou/infleansecurity/InflearnSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojinyou.infleansecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSecurityApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
